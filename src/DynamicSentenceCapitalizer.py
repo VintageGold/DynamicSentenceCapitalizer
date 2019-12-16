6d51590f0b3f2bd7abcd17608245799b717e8cce
VintageGold <52210449+VintageGold@users.noreply.github.com>
2019-12-16 12:42:12 -0500

    Update DynamicSentenceCapitalizer.py

--- src/DynamicSentenceCapitalizer.py
+++ src/DynamicSentenceCapitalizer.py
@@ -1,58 +1,61 @@
-def userInput():
-    userInput = input('Type in 3 sentences in lowercase, we will do the capitalization for you. ''\n')
-    readUserInput(userInput)
+def user_input():
+    user_text = input('Type in 3 sentences in lowercase, we will do the capitalization for you. ''\n')
+    read_user_input(user_text)
 
-#Capitalizes letter when char is the first alphabetical letter of a sentence
-def capitalizeLetter(letter):
+
+# Capitalizes letter when char is the first alphabetical letter of a sentence
+def capitalize_letter(letter):
     capital = letter.capitalize()
     return capital
 
-#Process User Input
-def readUserInput(updatedUserInput):
-    characterList = []
-    alphabet = False
-    alphaFound = 0
+
+# Process User Input
+def read_user_input(updated_user_input):
+    character_list = []
+    alpha_found = 0
     i = 0
-    indexCapitalize = 0
+    index_capitalize = 0
 
-    #Reads through all characters in userInput to
-    #determine what will be capitalized and which will not.
-    for char in updatedUserInput:
+    # Reads through all characters in userInput to
+    # determine what will be capitalized and which will not.
+    for char in updated_user_input:
 
-        #Capitalize first letter of entry
-        if indexCapitalize == i:
-            characterList.append(capitalizeLetter(char))
+        # Capitalize first letter of entry
+        if index_capitalize == i:
+            character_list.append(capitalize_letter(char))
 
-        #Capitalizes first found letter after every period
-        elif char == '.' or char == '!'or char == '?':
-            characterOffset = i + 1 #adds one to i so the period found in the loop before,
-                                    #does not appear in the search loop below
-            for subChar in updatedUserInput[characterOffset:]:
-                #Tests whether character after period is an alphabetical character and
-                #if so adds index to periodIndex
-                alphaFound = subChar.isalpha()
-                if alphaFound == True:
-                    indexCapitalize = updatedUserInput.find(subChar,i)
+        # Capitalizes first found letter after every period
+        elif char in ('.', '!', '?'):
+            character_offset = i + 1  # adds one to i so the period found in the loop before,
+            # does not appear in the search loop below
+            for sub_char in updated_user_input[character_offset:]:
+                # Tests whether character after period is an alphabetical character and
+                # if so adds index to periodIndex
+                alpha_found = sub_char.isalpha()
+                if alpha_found == True:
+                    index_capitalize = updated_user_input.find(sub_char, i)
                     break
 
-            #Adds letter to be capitalized to characterList
-            characterList.append(char)
+            # Adds letter to be capitalized to characterList
+            character_list.append(char)
 
         else:
-            #Adds letter to characterList
-            characterList.append(char)
+            # Adds letter to characterList
+            character_list.append(char)
         i += 1
 
-    #Reads through indexes of the letters to be capitalized
-    output(characterList)
+    # Reads through indexes of the letters to be capitalized
+    output(character_list)
 
-def output(updatedCharacterList):
-    updatedSentence =''
-    updatedSentence = updatedSentence.join(updatedCharacterList)
-    print(updatedSentence)
+
+def output(updated_characterlist):
+    updated_sentence = ''
+    updated_sentence = updated_sentence.join(updated_characterlist)
+    print(updated_sentence)
+
 
 def main():
-    userInput()
-    exit()
+    user_input()
+
 
 main()
